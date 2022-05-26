ae2e142e5b5ca84a180d7e953d69748fbb3d02b7
Lucky-victory <blizwonder25@gmail.com>
2022-05-26 18:11:44 +0100

    fixing prerender bug

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,2 +1,4 @@
+import { APP_BASE_HREF } from '@angular/common';
+import { InjectionToken } from '@angular/core';
 export const API_BASE_URL: string = 'https://blog-api-harpee.herokuapp.com';
-export const APP_BASE_URL: string = window.location.origin;
+export const APP_BASE_URL: InjectionToken<string> = APP_BASE_HREF;
--- src/app/single-article/single-article.component.ts
+++ src/app/single-article/single-article.component.ts
@@ -1,66 +1,74 @@
-
-
-import { Component, OnInit } from '@angular/core';
+import { Component, InjectionToken, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
-import { ActivatedRoute,  Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AppService } from '../app.service';
-import { APP_BASE_URL} from '../constants';
+import { APP_BASE_URL } from '../constants';
 
 import { ISingleArticle } from './single-article.type';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-article.component.html',
-  styleUrls: ['./single-article.component.css']
+  styleUrls: ['./single-article.component.css'],
 })
-
 export class SingleArticleComponent implements OnInit {
-  
   singleArticle!: ISingleArticle;
-baseUrl:string=APP_BASE_URL;
+  baseUrl: InjectionToken<string> = APP_BASE_URL;
 
-  constructor(private activeRoute:ActivatedRoute,private router:Router, private service:AppService,private pageTitle:Title, private pageMeta:Meta) { 
-
-  }
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private router: Router,
+    private service: AppService,
+    private pageTitle: Title,
+    private pageMeta: Meta
+  ) {}
 
   ngOnInit(): void {
-    let slug:string | null='';
-this.activeRoute.paramMap.subscribe(params=>{
-   slug=params.get('slug');
-  this.service.getSingleArticle(slug).subscribe(article => {
-    
-     this.singleArticle=article;
-     this.setTitle(this.singleArticle.title);
-    
-     this.setMetaTags(this.singleArticle)
-        });
+    let slug: string | null = '';
+    this.activeRoute.paramMap.subscribe((params) => {
+      slug = params.get('slug');
+      this.service.getSingleArticle(slug).subscribe((article) => {
+        this.singleArticle = article;
+        this.setTitle(this.singleArticle.title);
+
+        this.setMetaTags(this.singleArticle);
+      });
     });
-    
-}
- 
-  setTitle(newTitle:string){
+  }
+
+  setTitle(newTitle: string) {
     this.pageTitle.setTitle(newTitle);
   }
-  
-  
-  setMetaTags(singleArticle:ISingleArticle){
-    this.pageMeta.addTags([{
-      property:'og:url',content:`${APP_BASE_URL}/article/${this.singleArticle.slug}`
-    },{
-      property:'og:image',content:singleArticle.heroImage
-    },{
-           property:'og:title',content:singleArticle.title
-        },{
-          name:'twitter:creator',content:'@lucky_victory1'
-       },
-       {
-        name:'twitter:image',content:singleArticle.heroImage
-       },
-       {
-           property:'og:type',content:'website'
+
+  setMetaTags(singleArticle: ISingleArticle) {
+    this.pageMeta.addTags(
+      [
+        {
+          property: 'og:url',
+          content: `${APP_BASE_URL}/article/${this.singleArticle.slug}`,
         },
-       
-          ],true)
+        {
+          property: 'og:image',
+          content: singleArticle.heroImage,
+        },
+        {
+          property: 'og:title',
+          content: singleArticle.title,
+        },
+        {
+          name: 'twitter:creator',
+          content: '@lucky_victory1',
+        },
+        {
+          name: 'twitter:image',
+          content: singleArticle.heroImage,
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+      ],
+      true
+    );
   }
- 
 }
