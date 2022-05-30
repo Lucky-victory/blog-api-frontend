65271d715f67d5324fc316b6333d588fbb98286e
Lucky Victory <luckyvictory54@gmail.com>
2022-05-30 13:47:11 +0000

    [fix] SSR bugs, SSR now working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,8 +42,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: 'enabledBlocking',
-    }),
+    initialNavigation: 'enabledBlocking',
+}),
   ],
   exports: [RouterModule],
 })
